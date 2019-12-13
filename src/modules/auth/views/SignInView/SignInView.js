2c58a371ec3a27076639f4d7569e73c373052ea5
Agata Stawarz <agata.stawarz94@gmail.com>
2019-12-13 10:11:25 +0100

    Delete KeyboardAwayView

--- src/modules/auth/views/SignInView/SignInView.js
+++ src/modules/auth/views/SignInView/SignInView.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { View, KeyboardAvoidingView } from "react-native";
+import { View } from "react-native";
 
 import { authActions } from "../../redux/actions";
 
@@ -33,24 +33,22 @@ class SignInView extends Component {
     const { container, registerBox } = Styles;
 
     return (
-      <KeyboardAvoidingView style={{ flex: 1 }}>
-        <AuthContainer>
-          <View style={container}>
-            <HeaderTitle>Sign in</HeaderTitle>
-            <Input placeholder="e-mail" />
-            <Input placeholder="password" secureTextEntry={true} />
-            <ActionButton text="sign in" onPress={() => navigate("Home")} />
-          </View>
-          <View style={registerBox}>
-            <AppText>You don't have an account? </AppText>
-            <ActionButton
-              isLink
-              text="Sign up"
-              onPress={() => navigate("SignUp")}
-            />
-          </View>
-        </AuthContainer>
-      </KeyboardAvoidingView>
+      <AuthContainer>
+        <View style={container}>
+          <HeaderTitle>Sign in</HeaderTitle>
+          <Input placeholder="e-mail" />
+          <Input placeholder="password" secureTextEntry={true} />
+          <ActionButton text="sign in" onPress={() => navigate("Home")} />
+        </View>
+        <View style={registerBox}>
+          <AppText>You don't have an account? </AppText>
+          <ActionButton
+            isLink
+            text="Sign up"
+            onPress={() => navigate("SignUp")}
+          />
+        </View>
+      </AuthContainer>
     );
   }
 }
