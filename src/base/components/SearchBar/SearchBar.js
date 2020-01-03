1b82686eaef10bda8730bc4beea5d4271be9e4c5
Agata Stawarz <agata.stawarz94@gmail.com>
2020-01-03 15:44:32 +0100

    Add SearchBar Component

---
+++ src/base/components/SearchBar/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, TextInput } from 'react-native';
+import PropTypes from 'prop-types';
+import { Ionicons } from '@expo/vector-icons';
+import Styles from './Styles';
+
+const { container, input } = Styles;
+export default function SearchBar(props) {
+  return (
+    <View style={container}>
+      <Ionicons name="ios-search" size={24} color="grey" />
+      <TextInput
+        blurOnSubmit={false}
+        autoCapitalize="none"
+        style={input}
+        {...props}
+      />
+    </View>
+  );
+}
+
+SearchBar.propTypes = {
+  autoCompleteType: PropTypes.string,
+  errorMessage: PropTypes.string,
+  isPassword: PropTypes.bool,
+  isError: PropTypes.bool,
+  value: PropTypes.string.isRequired,
+};
+
+SearchBar.defaultProps = {
+  autoCompleteType: 'off',
+  errorMessage: '',
+  isError: false,
+  isPassword: false,
+};
---
+++ src/base/components/SearchBar/Styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+import Typography from '../../constants/Typography';
+import Colors from '../../constants/Colors';
+
+const {
+  fontSize: { big },
+} = Typography;
+const { gray } = Colors;
+
+export default StyleSheet.create({
+  input: {
+    fontFamily: 'dm-sans',
+    fontSize: big,
+    padding: 5,
+    width: '90%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  container: {
+    borderBottomWidth: 1,
+    borderBottomColor: gray,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 50,
+    margin: 16,
+  },
+  icon: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+});
