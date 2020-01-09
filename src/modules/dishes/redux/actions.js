6bc6275ff5fb0180ae802f5c404d12428dfb2fd4
Agata Stawarz <agata.stawarz94@gmail.com>
2020-01-09 11:47:52 +0100

    Add Get Dish action

--- src/modules/dishes/redux/actions.js
+++ src/modules/dishes/redux/actions.js
@@ -1,9 +1,11 @@
-/* eslint-disable implicit-arrow-linebreak */
 import { apiAction } from '../../../base/redux/utils';
 import { API } from '../../../common/utils/apiUrls';
 
 const prefix = '@@dishes/';
 
 export const GET_DISHES = `${prefix}GET_DISHES`;
+export const GET_DISH = `${prefix}GET_DISH`;
 
 export const getDishes = () => apiAction(GET_DISHES, 'GET', API.GET_DISHES);
+export const getDish = (id) =>
+  apiAction(GET_DISHES, 'GET', `${API.GET_DISH}/${id}`);
--- src/modules/dishes/redux/reducers.js
+++ src/modules/dishes/redux/reducers.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
-import { GET_DISHES } from './actions';
+import { GET_DISHES, GET_DISH } from './actions';
 import { apiReducer } from '../../../base/redux/utils';
 
 export default combineReducers({
   getDishes: apiReducer(GET_DISHES),
+  getDish: apiReducer(GET_DISH),
 });
